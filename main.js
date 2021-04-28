078b28143331acb11704447f02d703d9507b1feb
Romain KOENIG <romain.koenig@gmail.com>
2021-04-28 16:45:35 +0200

    Basic but running

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const fetch = require('node-fetch');
+const fsLibrary = require('fs');
+
 const { logging } = require("./utils");
 
 require('dotenv').config();
@@ -22,10 +24,63 @@ async function getProjectsFromTrello() {
 
 	const subjects = JSON.parse(await response.text());
 
-	logging(subjects);
+	//logging(subjects);
+
+	return subjects;
 
 }
 
-// PROGRAM STARTS HERE
 
-getProjectsFromTrello();
+async function createReportingFile(templateFile, printingFcn, topics, outputFile) {
+
+
+	fsLibrary.readFile(templateFile, (error, data) => {
+		// In case of a error throw err exception. 
+		if (error) {
+			throw err;
+		}
+		else {
+			const template = data.toString();
+			const content = template.replace('<!-- Content here -->', topics.map(printingFcn).join("\n\n"));
+
+			fsLibrary.writeFile(outputFile, content, (error) => {
+				// In case of a error throw err exception. 
+				if (error)
+					throw err;
+			});
+		}
+	})
+}
+
+const printCardPresentation = topic => {
+
+	let topicTags = [];
+	topic.labels.forEach(label => {
+		topicTags.push(label.name);
+	});
+
+	const separator = " / "
+
+	let tags = topicTags.reduce((list, current) => list.concat(current, separator), "");
+
+	// Deleting " - " at the end of the string
+	tags = tags.substring(0, tags.length - separator.length);
+
+	logging(tags);
+
+	return `
+	<section>\n
+		<h1>${topic.name}</h1>\n
+		<small>${tags.trim()}</small>\n
+		<p class="r-fit-text">${topic.desc.replace(/\n/g, '<br/>')}</p>\n
+	</section>`;
+}
+
+
+// PROGRAM STARTS HERE
+(async () => {
+
+	const topics = await getProjectsFromTrello();
+
+	createReportingFile("./template.html", printCardPresentation, topics, "./OUTPUT/Reporting.html");
+})();
